228e5998de6cb417ab86aca73f86d92da819a159
uCO43h6IIXSKU5ABnDcQ <a1exFed@bk.ru>
2018-04-08 00:59:17 +0300

    add port

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 'use strict';
+const fs=require('fs');
 const http=require('http');
+const server=http.createServer(function(request, response){
+  console.log(request.url);
+  const content=fs.readFileSync('index.html', 'utf8');
+  response.end(content);
+});
+
+server.listen(process.env.PORT || 3003);
+console.log('Server started!');
