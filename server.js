ff50f724b42606f1913eb191fd0f34a1767edfd3
uCO43h6IIXSKU5ABnDcQ <a1exFed@bk.ru>
2018-04-10 19:12:33 +0300

    add cherio

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 'use strict';
 const fs=require('fs');
 const http=require('http');
+const ceerio=require('cheerio');
+
 const server=http.createServer(function(request, response){
+  //response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {'Content-Type': 'text/html'});
   console.log(request.url);
   const content=fs.readFileSync('index.html', 'utf8');
   response.end(content);
