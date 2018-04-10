6d4febcb6a4ca49d3c81ffce737db37d89f71871
uCO43h6IIXSKU5ABnDcQ <a1exFed@bk.ru>
2018-04-10 21:02:03 +0300

    add transformHTML

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 'use strict';
 const fs=require('fs');
 const http=require('http');
-const ceerio=require('cheerio');
+const cheerio=require('cheerio');
+const urlUtils = require('url');
+
+function transformHTML(content, mess){
+  var params=urlUtils.parse(mess);
+  // console.dir(params);
+  let $=cheerio.load(content);
+  $('<li>url: '+mess+'</li>').appendTo('.box > ul');
+  for (let i of Object.getOwnPropertyNames(params)){
+    $('<li>'+i+': '+params[i]+'</li>').appendTo('.box > ul');
+  }
+  return $.html();
+}
 
 const server=http.createServer(function(request, response){
-  //response.writeHead(200, {'Content-Type': 'text/plain'});
   response.writeHead(200, {'Content-Type': 'text/html'});
-  console.log(request.url);
+  //console.log(request.url);
   const content=fs.readFileSync('index.html', 'utf8');
-  response.end(content);
+  response.end(transformHTML(content, request.url));
 });
 
 server.listen(process.env.PORT || 3003);
